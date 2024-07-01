766f3dd30362486085351dbd5de010c6324e9d14
Gabriel Knupp <gabrielknpp@gmail.com>
2024-07-01 20:48:16 -0300

    Criando a base dos componentes do blog.

--- angular-blog/src/app/app.module.ts
+++ angular-blog/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TitleDescriptionComponent } from './components/title-description/title-description.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TitleDescriptionComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-blog/src/app/components/title-description/title-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-description',
+  templateUrl: './title-description.component.html',
+  styleUrls: ['./title-description.component.css']
+})
+export class TitleDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
