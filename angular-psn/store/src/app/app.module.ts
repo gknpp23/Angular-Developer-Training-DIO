739a924ec14ba5d3c7a06c62a77a4e297f7ac8e9
Gabriel Knupp <gabrielknpp@gmail.com>
2024-07-23 20:31:06 -0300

    Criei os componentes principais da página

--- angular-psn/store/src/app/app.module.ts
+++ angular-psn/store/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BigCardGameComponent } from './components/big-card-game/big-card-game.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CardOffersComponent } from './components/card-offers/card-offers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BigCardGameComponent,
+    MenuBarComponent,
+    SidebarComponent,
+    CardOffersComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-psn/store/src/app/components/card-offers/card-offers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-offers',
+  templateUrl: './card-offers.component.html',
+  styleUrls: ['./card-offers.component.css']
+})
+export class CardOffersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
