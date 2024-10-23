6468b16adc3033ccbc4329110fc3096a50f247ac
Gabriel Knupp <gabrielknpp@gmail.com>
2024-10-23 20:50:53 -0300

    Implementação do ajuste automático na largura do conteúdo principal ao redimensionar a barra lateral.

--- angular-steam/src/app/components/sidebar/side-card/side-card.component.ts
+++ angular-steam/src/app/components/sidebar/side-card/side-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-side-card',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-card.component.css']
 })
 export class SideCardComponent implements OnInit {
+  @ViewChild('sidenav')
+  sidenav!: MatSidenav;
+  
+  // Propriedade que controla o estilo do container principal
+  isSidenavOpen: boolean = true;
 
+  toggleSidenav() {
+    this.sidenav.toggle();
+    this.isSidenavOpen = !this.isSidenavOpen; // Atualizar estado da sidebar
+  }
   constructor() { }
 
   ngOnInit(): void {
